47d93e78cd545c3d21628610bd1b661e79211664
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-01-28 19:11:04 +0530

    add express validator in login controller

--- backend/Controllers/loginUser.js
+++ backend/Controllers/loginUser.js
@@ -1,28 +1,34 @@
 import userModel from "../Schema/User.js";
-
+import { validationResult } from "express-validator";
 
 const loginUser = async (req, res) => {
- 
- 
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    // returns true if not empty
     const { email, password } = req.body;
     try {
       let userData = await userModel.findOne({ email });
 
       if (!userData) {
-        console.log('Acccount Not Found')
-        return res.status(401).json({ error: 'Try logging in with correct credentials' })
+        console.log("Acccount Not Found");
+        return res
+          .status(401)
+          .json({ error: "Try logging in with correct credentials" });
       }
 
       if (userData.password !== password) {
-        return res.status(401).json({ error: 'Try logging in with correct credentials' })
+        return res
+          .status(401)
+          .json({ error: "Try logging in with correct credentials" });
       }
 
       return res.json({ success: true });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.json({ success: false });
     }
- 
-
-}
+  } else {
+    res.send({ errors: result.array() });
+  }
+};
 export default loginUser;
